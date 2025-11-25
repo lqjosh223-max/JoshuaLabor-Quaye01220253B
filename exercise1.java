898f87aa1e75e92b968217b3985b54c7120acb12
PC-1 <lqjosh223@gmail.com>
2025-11-25 13:25:34 +0000

    exercise1.class added

---
+++ exercise1.java
@@ -0,0 +1,10 @@
+
+public class exercise1 {
+	public static void main(String[] args) {
+		
+		System.out.print("hello my name is Joshua Labor-Quaye ");
+		
+		System.out.print("I am learning Java BEE429");
+		
+	}
+}
