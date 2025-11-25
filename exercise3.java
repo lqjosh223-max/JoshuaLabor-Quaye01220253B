06cb0dde63bf4707d31f7e698523547abc2f7b37
lqjosh223-max <lqjosh223@gmail.com>
2025-11-25 14:07:30 +0000

    exercise3 added

---
+++ exercise3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class exercise3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter your Name: ");
+        String name = input.nextLine();
+        
+      
+        
+        System.out.println("Welcome " + name + " !." );
+    }
+}
